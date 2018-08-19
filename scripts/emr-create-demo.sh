e740feca51103be45d775cea3b83a58f06b0dfc2
reed9999 <reed9999@uw.edu>
2018-08-19 13:15:16 -0700

    004--starting to use eventcodes
    But more importantly, I want to parametrize my aws CLI shell script

---
+++ scripts/emr-create-demo.sh
@@ -0,0 +1,37 @@
+DEMO_NUMBER="004"
+#I would like to add more parametrization here -- see below.
+aws emr create-cluster --applications Name=Ganglia Name=Hadoop Name=Hive Name=Hue Name=Mahout Name=Pig Name=Tez --ec2-attributes '{"KeyName":"MainKeyPair","InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"subnet-4453270f","EmrManagedSlaveSecurityGroup":"sg-00286a3fc3b77d243","EmrManagedMasterSecurityGroup":"sg-03b92a0e407d74bab"}' --service-role EMR_DefaultRole --enable-debugging --release-label emr-5.16.0 --log-uri 's3n://philip-hadoop-bucket/' --steps '[{"Args":["hive-script","--run-hive-script","--args","-f",
+"s3://philip-hadoop-bucket/first-demo-query/scripts/demo-'$DEMO_NUMBER'.q",
+"-d","OUTPUT=s3://philip-hadoop-bucket/first-demo-query/output/003"],"Type":"CUSTOM_JAR","ActionOnFailure":"CONTINUE","Jar":"command-runner.jar","Properties":"","Name":"Demo 003 Count by actor country pairs"}
+]' --name 'GDELT '$DEMO_NUMBER' demo' \
+--instance-groups '[{"InstanceCount":1,"InstanceGroupType":"MASTER","InstanceType":"m3.xlarge","Name":"Master Instance Group"},{"InstanceCount":1,"InstanceGroupType":"CORE","InstanceType":"m3.xlarge","Name":"Core Instance Group"}]' \
+--scale-down-behavior TERMINATE_AT_TASK_COMPLETION --region us-west-2
+
+### SMALLER SCALE ATTEMPTS, NOT WORKING
+
+# INSTANCE_TYPE="m3.large" #m3.large may not be enough anyway
+# echo "This doesn't appear to work because everything is so thoroughly escaped:"
+# echo "$INSTANCE_TYPE"
+# INSTANCE_GROUPS='[{"InstanceCount":1,"InstanceGroupType":"MASTER","InstanceType":"m3.xlarge","Name":"Master Instance Group"},{"InstanceCount":1,"InstanceGroupType":"CORE","InstanceType":"m3.xlarge","Name":"Core Instance Group"}]'
+
+
+### FIRST FULL SCALE ATTEMPT, NOT WORKING
+
+# $DEMO_NUMBER="003"
+# $DEMO_DESCRIPTION="Count by actor country pairs"
+# $INSTANCE_TYPE="m3.large" #was m3.xlarge, also second InstanceCount was 2
+# $SCRIPT_TO_RUN="s3://philip-hadoop-bucket/first-demo-query/scripts/demo-$DEMO_NUMBER.q"
+# # $REGION="us-west-2"
+# $REGION="ap-south-1"  #Mumbai--to experiment, if not too expensive.
+# aws emr create-cluster --applications Name=Ganglia Name=Hadoop Name=Hive Name=Hue Name=Mahout Name=Pig Name=Tez --ec2-attributes '{"KeyName":"MainKeyPair","InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"subnet-4453270f","EmrManagedSlaveSecurityGroup":"sg-00286a3fc3b77d243","EmrManagedMasterSecurityGroup":"sg-03b92a0e407d74bab"}' --service-role EMR_DefaultRole --enable-debugging --release-label emr-5.16.0 --log-uri 's3n://philip-hadoop-bucket/' --steps '[
+#   {
+#     "Args":[
+#       "hive-script","--run-hive-script","--args","-f",
+#       $SCRIPT_TO_RUN,
+#       "-d",
+#       "OUTPUT=s3://philip-hadoop-bucket/first-demo-query/output/003"
+#     ],
+#     "Type":"CUSTOM_JAR","ActionOnFailure":"CONTINUE","Jar":"command-runner.jar","Properties":"","Name":"Demo $DEMO_NUMBER $DEMO_DESCRIPTION"
+#     }
+# ]' --name 'GDELT Demo $DEMO_NUMBER' --instance-groups '[{"InstanceCount":1,"InstanceGroupType":"MASTER","InstanceType":"$INSTANCE_TYPE","Name":"Master Instance Group"},{
+#     "InstanceCount":1,"InstanceGroupType":"CORE","InstanceType":"$INSTANCE_TYPE","Name":"Core Instance Group"}]' --scale-down-behavior TERMINATE_AT_TASK_COMPLETION --region us-west-2
