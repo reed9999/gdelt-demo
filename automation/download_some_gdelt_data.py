284250d3998225116300d6142672f55c011f538a
reed9999 <reed9999@uw.edu>
2018-09-27 20:47:16 -0700

    Extract the download boto3 code to its own file under automation.

---
+++ automation/download_some_gdelt_data.py
@@ -0,0 +1,44 @@
+####
+# Since 003 didn't enhance much, I'm continuing to work as 003 locally.
+# Weirdly I can't figure out how I was running Pyspark stuff locally. I don't 
+# seem to have pyspark libs installed in any virtualenvs. Time to fix that. 
+
+import os
+from os.path import exists
+import re 
+import boto3
+
+if (exists("/home/philip")):
+    IS_LOCAL = True
+
+###
+# Eventually this should probably be refactored to the automation directory
+# Code is taken from https://stackoverflow.com/q/31918960/
+# My adaptation isn't great though because it downloads stray directories
+# not matching the pattern.
+
+client = boto3.client('s3')
+resource = boto3.resource('s3')
+def download_dir(client, resource, dist, 
+    local='/home/philip/aws/data/original', 
+    bucket='reed9999',
+    pattern='.*', ):
+
+    paginator = client.get_paginator('list_objects')
+    for result in paginator.paginate(Bucket=bucket, Delimiter='/', Prefix=dist):
+        if result.get('CommonPrefixes') is not None:
+            for subdir in result.get('CommonPrefixes'):
+                download_dir(client, resource, subdir.get('Prefix'), local, bucket)
+        if result.get('Contents') is not None:
+            for file in result.get('Contents'):
+                if not os.path.exists(os.path.dirname(local + os.sep + file.get('Key'))):
+                     os.makedirs(os.path.dirname(local + os.sep + file.get('Key')))
+                if re.search(pattern, file.get('Key')):
+                    resource.meta.client.download_file(bucket, file.get('Key'), local + os.sep + file.get('Key'))
+
+if IS_LOCAL:
+    download_dir(client, resource, 
+        dist='events/', 
+        bucket='gdelt-open-data',
+        pattern='1985\.')
+    print ("Downloaded")
--- queries/spark_sql/spark_003_local.py
+++ queries/spark_sql/spark_003_local.py
@@ -30,31 +30,6 @@ events = hiveCtx.read.format("com.databricks.spark.csv").option("delimiter",
 ev2 = events.take(2)
 
 
-###
-# Eventually this should probably be refactored to the automation directory
-# Code is taken from https://stackoverflow.com/q/31918960/
-client = boto3.client('s3')
-resource = boto3.resource('s3')
-def download_dir(client, resource, dist, local='/tmp', bucket='reed9999',
-    pattern='.*', ):
-
-    paginator = client.get_paginator('list_objects')
-    for result in paginator.paginate(Bucket=bucket, Delimiter='/', Prefix=dist):
-        if result.get('CommonPrefixes') is not None:
-            for subdir in result.get('CommonPrefixes'):
-                download_dir(client, resource, subdir.get('Prefix'), local, bucket)
-        if result.get('Contents') is not None:
-            for file in result.get('Contents'):
-                if not os.path.exists(os.path.dirname(local + os.sep + file.get('Key'))):
-                     os.makedirs(os.path.dirname(local + os.sep + file.get('Key')))
-                if re.search(pattern, file.get('Key')):
-                    resource.meta.client.download_file(bucket, file.get('Key'), local + os.sep + file.get('Key'))
-
-if IS_LOCAL:
-    download_dir(client, resource, dist='events/', local='/tmp', bucket='gdelt-open-data',
-        pattern='1985\.')
-    print ("Downloaded")
-
 if not IS_LOCAL:
     with open("/home/hadoop/output_003.txt", "w") as f:
         f.write(str(len(ev2)))
