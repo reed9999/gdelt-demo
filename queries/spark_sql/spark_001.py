55dc4079746ed38890f9fc93c68054f15a133d2f
reed9999 <reed9999@uw.edu>
2018-10-01 11:09:27 -0700

    Clarification on what this is. I only intended it to be pastein, I think

--- queries/spark_sql/spark_001.py
+++ queries/spark_sql/spark_001.py
@@ -2,4 +2,13 @@
 # First demo script for spark-sql to explore the spark-submit workflow that
 # I'll be using in production.
 
+
+# Neither one of these works as a standalone Python script unless PySpark is
+# installed separately. I think my original intent was to have a script I could
+# paste in at the command prompt. At any rate, this minimal case is *too*
+# minimal to still be useful for troubleshoot.
+
+# Before I had
 import spark.sql.HiveContext
+# In other contexts I have
+#import pyspark.sql.HiveContext
