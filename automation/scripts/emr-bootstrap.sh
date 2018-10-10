b1a434e6d800f525fb4d2c78370e465f88208367
reed9999 <reed9999@uw.edu>
2018-10-10 12:25:30 -0700

    UNTESTED: EMR Cluster setup to run feature extraction on small DB

--- automation/scripts/emr-bootstrap.sh
+++ automation/scripts/emr-bootstrap.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+# Common setup tasks to configure my AWS Elastic MapReduce (EMR)
+# I could separate out the Spark versus Hadoop setup but no real reason.
+
+# SPARK SETUP
+# Some of this is redundant if the GitHub cloning below works as expected.
 mkdir scripts
 aws s3 cp s3://reed9999/queries/spark_sql/spark_001.py /home/hadoop/scripts/spark_001.py
 aws s3 cp s3://reed9999/queries/spark_sql/spark_002.py /home/hadoop/scripts/spark_002.py
@@ -6,3 +12,11 @@ aws s3 cp s3://reed9999/queries/spark_sql/spark_002.py /home/hadoop/scripts/spar
 aws s3 cp s3://reed9999/queries/spark_sql/__spark_002_standalone.py /home/hadoop/scripts/spark_002_standalone
 aws s3 cp s3://reed9999/queries/spark_sql/spark_003.py /home/hadoop/scripts/spark_003.py
 pip install pyspark
+
+
+# HADOOP SETUP
+sudo apt install git
+git clone http://reed9999/gdelt-demo
+ln -s gdelt-demo demo
+
+
---
+++ automation/scripts/emr-cluster-3.1.sh
@@ -0,0 +1,16 @@
+## Script to create a cluster that's very much like the one I called 3.1 on 2018-10-09
+# As it gets more parametrized my naming convention may change. 
+aws emr create-cluster \
+--applications Name=Ganglia Name=Hadoop Name=Hive Name=Hue Name=Mahout Name=Pig Name=Tez \
+--tags 'WhatsNew=Parameters for smaller DB' 'Hadoop=1' 'CliAutomation=1' \
+--ec2-attributes InstanceProfile=EMR_EC2_DefaultRole \
+--release-label emr-5.16.0 --log-uri 's3n://philip-hadoop-bucket/__archive/' \
+--bootstrap-actions Path="s3://reed9999/automation/scripts/emr-bootstrap.sh" \
+--steps '[
+  {"Args":["hive-script", "--run-hive-script", "--args", "-f","s3://reed9999/queries/data-engineering/hive/load-mini-gdelt-events.q", "-d","INPUT=s3://reed9999/events/_small", "-d","OUTPUT=s3://reed9999/output/step0.0","-hiveconf", "hive.support.sql11.reserved.keywords=false"],"Type":"CUSTOM_JAR","ActionOnFailure":"CONTINUE","Jar":"command-runner.jar","Properties":"","Name":"step 0.0 load small subset of DB"},
+  {"Args":["hive-script","--run-hive-script","--args","-f","s3://reed9999/queries/feature_extraction/dyad_classification_features.q","-d","INPUT=s3://reed9999/data/subset/small","-d","OUTPUT=s3://reed9999/output/step0.1","-hiveconf","hive.support.sql11.reserved.keywords=false"],"Type":"CUSTOM_JAR","ActionOnFailure":"CONTINUE","Jar":"command-runner.jar","Properties":"","Name":"step 1.0 last cluster grand finale, failed because of forgotten DROP TABLE"}]' \
+--instance-groups '[{"InstanceCount":1,"BidPrice":"OnDemandPrice","InstanceGroupType":"CORE","InstanceType":"m1.large","Name":"Core Instance Group"},
+  {"InstanceCount":1,"BidPrice":"OnDemandPrice","InstanceGroupType":"MASTER","InstanceType":"m1.large","Name":"Master Instance Group"}]' \
+--ebs-root-volume-size 10 --service-role EMR_DefaultRole \
+--enable-debugging --name '3.2 (replica of 3.1) Fixed missing DROP TABLE ' \
+--scale-down-behavior TERMINATE_AT_TASK_COMPLETION --region us-west-2
