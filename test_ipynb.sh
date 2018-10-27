dac65a7d1b2532b4acf2d4c6ffd62fea3e86d2e0
Philip J. Reed <reed9999@uw.edu>
2018-10-26 23:34:28 -0700

    Convert hyphen to underscore in test_ipynb.sh. One test file I overlooked.

--- test_ipynb.sh
+++ test_ipynb.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
   # If executing the Jupyter notebook conversion doesn't error, then the
   # user should be able to progress through the notebook without error
-jupyter-nbconvert --execute --to html Start-here.ipynb --ExecutePreprocessor.timeout=300
+jupyter-nbconvert --execute --to html Start_here.ipynb --ExecutePreprocessor.timeout=300
 rm Start-here.html
