19d801a93008930471ae1520807a37806556f9da
Philip J. Reed <reed9999@uw.edu>
2018-10-26 23:44:03 -0700

    Convert another hyphen to underscore. I changed the first occurrence and missed the second, when I clean up the converted file.

--- test_ipynb.sh
+++ test_ipynb.sh
@@ -2,4 +2,4 @@
   # If executing the Jupyter notebook conversion doesn't error, then the
   # user should be able to progress through the notebook without error
 jupyter-nbconvert --execute --to html Start_here.ipynb --ExecutePreprocessor.timeout=300
-rm Start-here.html
+rm Start_here.html
