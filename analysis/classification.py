16395c56e9d6ed718058018876271acd7b58f86c
Philip J. Reed <>
2018-12-15 12:21:25 -0800

    REFACTOR: derived classes for classifiers Now instantiate the specific kind of classifier and run go()

--- analysis/classification.py
+++ analysis/classification.py
@@ -33,13 +33,86 @@ THIS_FILE_DIR = os.path.dirname(__file__)
 # so each kind of classification "just works."
 
 
-class GdeltClassificationTask():
+class GdeltClassificationTask:
 
     def load_data(self):
         self._dataframe = get_country_features()
 
+    def go(self):
+        raise NotImplementedError("Abstract base class; implement with a derived class.")
 
-    def do_decision_tree(self):
+class GdeltSvmTask(GdeltClassificationTask):
+    def do_svm_sample(self):
+        """ Adapting https://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVC.html
+        My earlier attempt to adapt to the iris data (see above) was bothering me so I decided
+        to go back to this much simpler example.
+        """
+
+        X = [[0], [1], [2], [3]]
+        Y = [0, 1, 2, 3]
+        self._classifier.fit(X, Y)
+        decision_function = self._classifier.decision_function([[1]])
+        assert decision_function is not None
+        assert decision_function.shape[1] > 0
+        print ("Classifier coef for SVM:\n{}".format(self._classifier.coef_))
+
+        # See https://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVC.html#sklearn.svm.LinearSVC.decision_function
+        # I forget which example I picked up the following code from but clearly I need to
+        # implement it better because this errors with  ValueError: X has 1 features per sample; expecting 4
+        # decision_function = self._classifier.decision_function([[1]])
+        # assert decision_function is not None
+        # assert decision_function.shape[1] > 0
+
+    def go(self):
+        """As with do_decision_tree and with other analysis, I'll start by
+        using Scikit-learn sample data and pasted in code."""
+        print ("\n*****        SVM          *****")
+        self._classifier = LinearSVC(random_state=0, tol=1e-5)
+        self.do_svm_sample()
+
+class GdeltRandomForestTask(GdeltClassificationTask):
+    def do_random_forest_sample(self):
+        """
+        As above, just a sanity check using sample code from Scikit-learn documentation
+        :return:
+        """
+        print("\n*****    RANDOM FOREST    *****")
+        clf = self._classifier
+        X = [[0, 0], [1, 1], [0.1, 0.1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1],]
+        Y = [0, 1, 0, 1, 1, 1, 1, 1, 1, 1,]
+        clf = clf.fit(X, Y)
+
+        scores = cross_val_score(clf, X, Y, cv=2)
+        assert scores is not None
+        assert len(scores) > 0
+        print(scores)
+
+    def go(self):
+        self._classifier = RandomForestClassifier(n_estimators=10)
+        self.do_random_forest_sample()
+
+class GdeltKnnTask(GdeltClassificationTask):
+    def do_knn_sample(self):
+        """
+        As above, just a sanity check using sample code from Scikit-learn documentation
+        :return:
+        """
+        print("\n*****        KNN         *****")
+        X = [[0], [1], [2], [3]]
+        y = [0, 0, 1, 1]
+        self._classifier = self._classifier.fit(X, y)
+        clf = self._classifier
+        assert clf.predict([[1.1]]) is not None
+        assert clf.predict_proba([[0.9]]) is not None
+        print(clf.predict([[1.1]]))
+        print(clf.predict_proba([[0.9]]))
+
+    def go(self):
+        self._classifier = KNeighborsClassifier(n_neighbors=3)
+        self.do_knn_sample()
+
+class GdeltDecisionTreeTask(GdeltClassificationTask):
+    def go(self):
         self.load_data()
         self._dataframe.dropna()
         # TODO There is a bogus column called Unnamed: 62 that is full of nan. I
@@ -83,137 +156,6 @@ class GdeltClassificationTask():
         #This only displays the tree within the Jupyter Notebook environment
         display(SVG(graph.pipe(format='svg')))
 
-    def do_svm_sample(self):
-        """ Adapting https://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVC.html
-        My earlier attempt to adapt to the iris data (see above) was bothering me so I decided
-        to go back to this much simpler example.
-        """
-
-        X = [[0], [1], [2], [3]]
-        Y = [0, 1, 2, 3]
-        self._classifier.fit(X, Y)
-        decision_function = self._classifier.decision_function([[1]])
-        assert decision_function is not None
-        assert decision_function.shape[1] > 0
-        print ("Classifier coef for SVM:\n{}".format(self._classifier.coef_))
-
-        # See https://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVC.html#sklearn.svm.LinearSVC.decision_function
-        # I forget which example I picked up the following code from but clearly I need to
-        # implement it better because this errors with  ValueError: X has 1 features per sample; expecting 4
-        # decision_function = self._classifier.decision_function([[1]])
-        # assert decision_function is not None
-        # assert decision_function.shape[1] > 0
-
-    def do_svm(self):
-        """As with do_decision_tree and with other analysis, I'll start by
-        using Scikit-learn sample data and pasted in code."""
-        print ("\n*****        SVM          *****")
-        self._classifier = LinearSVC(random_state=0, tol=1e-5)
-        self.do_svm_sample()
-
-    def do_random_forest_sample(self):
-        """
-        As above, just a sanity check using sample code from Scikit-learn documentation
-        :return:
-        """
-        print("\n*****    RANDOM FOREST    *****")
-        clf = self._classifier
-        X = [[0, 0], [1, 1], [0.1, 0.1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1],]
-        Y = [0, 1, 0, 1, 1, 1, 1, 1, 1, 1,]
-        clf = clf.fit(X, Y)
-
-        scores = cross_val_score(clf, X, Y, cv=2)
-        assert scores is not None
-        assert len(scores) > 0
-        print(scores)
-
-    def do_random_forest(self):
-        self._classifier = RandomForestClassifier(n_estimators=10)
-        self.do_random_forest_sample()
-
-    def do_knn_sample(self):
-        """
-        As above, just a sanity check using sample code from Scikit-learn documentation
-        :return:
-        """
-        print("\n*****        KNN         *****")
-        X = [[0], [1], [2], [3]]
-        y = [0, 0, 1, 1]
-        self._classifier = self._classifier.fit(X, y)
-        clf = self._classifier
-        assert clf.predict([[1.1]]) is not None
-        assert clf.predict_proba([[0.9]]) is not None
-        print(clf.predict([[1.1]]))
-        print(clf.predict_proba([[0.9]]))
-
-    def do_knn(self):
-        self._classifier = KNeighborsClassifier(n_neighbors=3)
-        self.do_knn_sample()
-
-    #####
-    # DECISION TREE DEMOS
-    # These are just part of the sanity check and can be deleted when no longer helpful for reference.
-    #
-    # def do_decision_tree_simple_demo(self):
-    #     """Demo taken from http://dataaspirant.com/2017/02/01/decision-tree-algorithm-python-with-scikit-learn/
-    #     Still just a sanity check."""
-    #     balance_data = pd.read_csv(
-    #         'https://archive.ics.uci.edu/ml/machine-learning-databases/balance-scale/balance-scale.data',
-    #         sep=',', header=None)
-    #     X = balance_data.values[:, 1:5]
-    #     y = balance_data.values[:, 0]
-    #
-    #     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
-    #     clf = self._classifier
-    #     comparison_tuple = (
-    #         "This fit passes in comparison to the other one",
-    #         y_train,
-    #         type(y_train),
-    #         type(y),
-    #         len(y),
-    #         y[0],
-    #     )
-    #     clf.fit(X_train, y_train)
-    #     score = self.do_decision_tree_demo_output(X_test, y_test)
-    #     return score
-    #
-    # def do_decision_tree_demo_output(self, X_test, y_test):
-    #     """Continuing to refactor the demo from
-    #     http://dataaspirant.com/2017/02/01/decision-tree-algorithm-python-with-scikit-learn/
-    #     just a bit.
-    #     """
-    #     what_to_predict = [[4, 4, 3, 3,]]
-    #     one_pred = self._classifier.predict(what_to_predict)
-    #     print ("Value {} - prediction {}".format(what_to_predict, one_pred))
-    #     if one_pred == "R":
-    #         print(
-    #             """This illustrates an important point about decision trees. [4, 4, 3, 3,]
-    #             means the balance tilts to the left because 4*4 > 3*3 but I predicted right.""")
-    #         print(
-    #             """Why? Because this example is a bit of an outlier (high numbers) and for whatever reason
-    #             the tree starts out evaluating R. Most of the time a high R weight and R distance would of
-    #             course be predictive of R. Note the much higher gini in the extreme lower right node though
-    #             even though it is still R, which is incorrect in this example.""")
-    #
-    #     another_pred = self._classifier.predict([[2, 2, 1, 1,]])
-    #     print ("What about [2, 2, 1, 1,] (should be L)? I predict {}".format(another_pred))
-    #     our_score = accuracy_score(y_test, self._classifier.predict(X_test))
-    #
-    #     ya_pred = self._classifier.predict([[1, 1, 1, 1,]])
-    #     print ("What about [1, 1, 1, 1,] (should be B)? I predict {}".format(ya_pred))
-    #     our_score = accuracy_score(y_test, self._classifier.predict(X_test))
-    #
-    #     feature_names=['L weight', 'L distance', 'R weight', 'R distance']
-    #     # critically, since sklearn appears to sort the classes alphabetically in exporting to graphviz, we need to sort
-    #     # them too.
-    #     class_names=['B', 'L', 'R']
-    #     graph = Source(export_graphviz(self._classifier, out_file=None, class_names=class_names,
-    #                            feature_names=feature_names, filled=True))
-    #     #This only displays the tree within the Jupyter Notebook environment
-    #     display(SVG(graph.pipe(format='svg')))
-    #
-    #     return our_score
-
     def do_decision_tree_demos(self):
         print ("\n*****    DECISION TREE    *****")
 
@@ -230,9 +172,9 @@ class GdeltClassificationTask():
         at the moment. I need to think more about why this is.""")
 
 if __name__ == "__main__":
-    task = GdeltClassificationTask()
-    # task.do_decision_tree_demos()
-    task.do_decision_tree()
+    task = GdeltDecisionTreeTask()
+    task.go()
+    task = 'something_else'
     # task.do_svm()
     # task.do_random_forest()
     # task.do_knn()
