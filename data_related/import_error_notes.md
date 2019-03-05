d20769977b3cb4a4e8347130948b63338d4e97eb
Philip J. Reed <>
2019-03-04 16:38:28 -0800

    INTERMEDIATE. Imports still failing because MySQL and my output disagree on tab character."

---
+++ data_related/import_error_notes.md
@@ -0,0 +1,12 @@
+## Import error notes
+
+I'm presently using MySQL Workbench to import the tab-delimited files into my DB on 
+AWS.
+When it fails halfway through the data, I'd like to keep a record--
+I can get some value as a demo/dry run just from
+charging ahead rather than digging into the troubleshooting, but ultimately I need to come back 
+and fix these soon.
+
+### dyad_events_by_year
+
+import 1: loaded 2982 but it doesn't seem to have been the *first* 2982.
--- queries/data_engineering/sql/mysql_manually/create_features_tables.sql
+++ queries/data_engineering/sql/mysql_manually/create_features_tables.sql
@@ -11,13 +11,44 @@
 -- or load via stdin on the command prompt.
 
 
+DROP TABLE IF EXISTS dyad_events_by_year;
 CREATE TABLE dyad_events_by_year
-    (year int, 
-    actor1code text, 
-    actor2code text, 
+    (
+    year int,
+    actor1code text,
+    actor2code text,
     eventcode text,
-    eventbasecode text,  
-    eventrootcode text, 
-    goldsteinscale float, 
-    event_count int);
+    eventbasecode text,
+    eventrootcode text,
+    goldsteinscale float,
+    event_count int,
+    -- Not sure an id column adds value, but any cost should be negligible.
+    -- making it last may help with import: https://stackoverflow.com/questions/2463542
+    id int NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id)	
+    );
 
+-- The problem here is that it's a "tab-delimited" CSV file that resulted from my HiveQL, but it's not a 
+-- tab character that MySQL recognizes as \t.
+
+-- https://stackoverflow.com/q/6017032
+-- LOAD DATA LOCAL INFILE '<whatever>/000000_0.csv'
+LOAD DATA LOCAL INFILE '<whatever>/dyad_features_by_year/tiny1.csv'
+INTO TABLE dyad_events_by_year
+FIELDS TERMINATED BY '^A' 
+-- These should be default
+ENCLOSED BY '' ESCAPED BY '\\'
+LINES TERMINATED BY '\n' STARTING BY ''
+(
+    year,
+    actor1code,
+    actor2code,
+    eventcode,
+    eventbasecode,
+    eventrootcode,
+    goldsteinscale,
+    event_count
+    )
+SET id = NULL;
+
+SELECT * FROM dyad_events_by_year WHERE actor1code = 'AFR';
