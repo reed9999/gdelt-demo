8ff12c682733924626073483fb11effad6d1e50d
Philip J. Reed <>
2019-03-11 22:40:41 -0700

    Tweaks to alias script for virtualenv.

--- alias-activate.sh
+++ alias-activate.sh
@@ -1,16 +1,16 @@
+# This is a little utility script to make it easier to activate the virtualenv
+# Change the env vars to match your own directory structure and preference for alias name.
+
 # Be sure to run this by sourcing. Trying to run it from the command line creates a new shell
 # that terminates immediately.
 # E.g. if the ALIAS_NAME is act then run as follows: 
 # $ source alias-activate.sh ; act
 
-# This is a little utility script to make it easier to activate the virtualenv
-# Change the env vars to match your own directory structure and preference for alias name.
+# I'm tolerating a bit of DRY VIOLATION in order to have a self-contained one in each project.
 
 VIRTUALENVS=~/.virtualenvs
 VENV_NAME=gdelt-demo
 ALIAS_NAME=act
 alias $ALIAS_NAME="source $VIRTUALENVS/$VENV_NAME/bin/activate"
-
-# My own bash failure--can't figure out why the next line doesn't invoke the alias.
-
-# $ALIAS_NAME
+echo "You now have an alias that can be used to access this virtual env from a prompt like so:"
+echo "$ $ALIAS_NAME"
